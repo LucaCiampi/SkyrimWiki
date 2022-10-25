d2d0e559bc3b843431f396822d1f7de968bd5a67
Luca <luca.ciampi@edu.gobelins.fr>
2022-10-25 21:45:04 +0200

    added homepage controller and view

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class HomeController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('pages/home.html.twig');
+    }
+}
